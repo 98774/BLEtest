97d3b110466dd317b52cc302530e639f97f09a50
snyde247 <j.snyder98774@gmail.com>
2023-03-28 11:42:53 -0400

    Successfully finds LED controller with BLE filter

--- app/src/main/java/com/example/bletest/MainActivity.java
+++ app/src/main/java/com/example/bletest/MainActivity.java
@@ -3,9 +3,16 @@ package com.example.bletest;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
 import android.Manifest;
 import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.le.BluetoothLeScanner;
+import android.bluetooth.le.ScanCallback;
+import android.bluetooth.le.ScanFilter;
+import android.bluetooth.le.ScanResult;
+import android.bluetooth.le.ScanSettings;
 import android.content.BroadcastReceiver;
 import android.content.Context;
 import android.content.Intent;
@@ -14,94 +21,105 @@ import android.content.pm.PackageManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     Button scanButton;
     TextView scanText;
     TextView debugText;
-    IntentFilter scanIntentFilter = new IntentFilter(BluetoothAdapter.ACTION_SCAN_MODE_CHANGED);
-    BroadcastReceiver scanModeReciever = new BroadcastReceiver() {
-        @Override
-        public void onReceive(Context context, Intent intent) {
-            String action = intent.getAction();
-            if (action.equals(BluetoothAdapter.ACTION_SCAN_MODE_CHANGED)) {
-                int modeValue = intent.getIntExtra(BluetoothAdapter.EXTRA_SCAN_MODE, BluetoothAdapter.ERROR);
 
-                if (modeValue == BluetoothAdapter.SCAN_MODE_CONNECTABLE) {
-                    scanText.setText("The device is not in discoverable mode but can still recieve conections");
-                } else if (modeValue == BluetoothAdapter.SCAN_MODE_CONNECTABLE_DISCOVERABLE) {
-                    scanText.setText("The device is in discoverable mode");
-                } else if (modeValue == BluetoothAdapter.SCAN_MODE_NONE) {
-                    scanText.setText("The device is in discoverable mode and cannot recieve connections");
-                } else {
-                    scanText.setText("Error");
-                }
+    private final ScanCallback scanCallback = new ScanCallback() {
+        @Override
+        public void onScanResult(int callbackType, ScanResult result) {
+            BluetoothDevice device = result.getDevice();
+            // ...do whatever you want with this found device
+            if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
+                ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.BLUETOOTH_CONNECT}, 3);
             }
+            scanText.setText(device.getName());
+        }
+
+        @Override
+        public void onBatchScanResults(List<ScanResult> results) {
+            // Ignore for now
+        }
+
+        @Override
+        public void onScanFailed(int errorCode) {
+            debugText.setText("Nothing Found");
         }
     };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        int permissionCheck = ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION);
+        if (permissionCheck != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.ACCESS_FINE_LOCATION}, 4);
+        } else {
+            // if(areLocationServicesEnabled(this))
+            debugText.setTextColor(255);
+            debugText.setText("Permission not there");
+        }
+        BluetoothAdapter adapter = BluetoothAdapter.getDefaultAdapter();
+        BluetoothLeScanner scanner = adapter.getBluetoothLeScanner();
+
+        String[] names = new String[]{"LED Controller"};
+        List<ScanFilter> filters = null;
+        if (names != null) {
+            filters = new ArrayList<>();
+            for (String name : names) {
+                ScanFilter filter = new ScanFilter.Builder()
+                        .setDeviceName(name)
+                        .build();
+                filters.add(filter);
+            }
+        }
+
+        ScanSettings scanSettings = new ScanSettings.Builder()
+                .setScanMode(ScanSettings.SCAN_MODE_LOW_LATENCY)
+                .setCallbackType(ScanSettings.CALLBACK_TYPE_ALL_MATCHES)
+                .setMatchMode(ScanSettings.MATCH_MODE_AGGRESSIVE)
+                .setNumOfMatches(ScanSettings.MATCH_NUM_ONE_ADVERTISEMENT)
+                .setReportDelay(0L)
+                .build();
+
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         scanButton = (Button) findViewById(R.id.scanButton);
         scanText = (TextView) findViewById(R.id.scanText);
         debugText = (TextView) findViewById(R.id.debugText);
+        List<ScanFilter> finalFilters = filters;
         scanButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
 
-                //broadcast intent to other devices
-                Intent discoverableIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_DISCOVERABLE);
-                discoverableIntent.putExtra(BluetoothAdapter.EXTRA_DISCOVERABLE_DURATION, 8);
-                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_CONNECT) != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.BLUETOOTH_CONNECT}, 2);
-                    debugText.setText("Requesting permissions");
-                    scanText.setText("DSFf");
+
+                if (ActivityCompat.checkSelfPermission(MainActivity.this, Manifest.permission.BLUETOOTH_SCAN) != PackageManager.PERMISSION_GRANTED) {
+                    ActivityCompat.requestPermissions(MainActivity.this, new String[]{Manifest.permission.BLUETOOTH_SCAN}, 2);
                 }
 
-                startActivity(discoverableIntent);
+//                scanner.startScan(finalFilters, scanSettings, scanCallback);
 
-                Thread2 t=new Thread2();
-                t.start();
+                if (scanner != null) {
+                    scanner.startScan(finalFilters, scanSettings, scanCallback);
+                    scanText.setText("Scan Satret");
+                    //Log.d(TAG, "scan started");
+                }  else {
+                    scanText.setText("could not get scanner object");
+                    //Log.e(TAG, "could not get scanner object");
+                }
+
+                //broadcast intent to other devices
+
             }
         });
-
-        registerReceiver(scanModeReciever, scanIntentFilter);
-    }
-
-    Handler handler=new Handler(new Handler.Callback() {
-        @Override
-        public boolean handleMessage(@NonNull Message message) {
-            debugText.setText(String.valueOf((message.arg1)));
-            return false;
-        }
-    });
-    private class Thread2 extends Thread
-    {
-        public void run()
-        {
-            for(int i = 0 ; i < 50 ; i++){
-                //This doesn't work:
-                // debugText.setText(String.valueOf(i));
-
-                //Do this instead
-                Message message=Message.obtain();
-                message.arg1=i;
-                handler.sendMessage(message);
-
-
-                try {
-                    sleep(500);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
-
-            }
-        }
     }
 }
